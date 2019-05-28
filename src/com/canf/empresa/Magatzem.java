9180b7ec3f32f4ff59505f82f2cebc057b7df1c5
antonialoy <antonialoytorrens@github.com>
2019-05-28 16:08:04 +0200

    Retocat el main per fer proves i afegit alguns atributs extra a Magatzem.

--- src/com/canf/empresa/Magatzem.java
+++ src/com/canf/empresa/Magatzem.java
@@ -9,6 +9,7 @@ import com.canf.articles.Article;
 import com.canf.articles.Disc;
 import com.canf.articles.Llibre;
 import com.canf.articles.Pelicula;
+import com.canf.articles.Venta;
 import static com.canf.articles.tipusArticle.DISC;
 import static com.canf.articles.tipusArticle.LLIBRE;
 import static com.canf.articles.tipusArticle.PELÍCULA;
@@ -19,14 +20,17 @@ import java.util.ArrayList;
  * @author antonialoy
  */
 public class Magatzem {
+
     private int codi;
     private String nom;
     private ArrayList<Article> llistaArticles;
+    private ArrayList<Venta> historialVentes;
 
-    public Magatzem(String nom, int codi, ArrayList<Article> llistaArticles) {
+    public Magatzem(int codi, String nom, ArrayList<Article> llistaArticles, ArrayList<Venta> historialVentes) {
         this.setNom(nom);
         this.codi = codi;
         this.llistaArticles = new ArrayList<>();
+        this.historialVentes = new ArrayList<>();
     }
 
     public String getNom() {
@@ -44,36 +48,17 @@ public class Magatzem {
     public ArrayList<Article> getLlistaArticles() {
         return llistaArticles;
     }
+
+    public ArrayList<Venta> getHistorialVentes() {
+        return historialVentes;
+    }
     
+
     public Disc obtenirDisc(String interpret) {
         for (Article x : llistaArticles) {
-            if (x.getTipusArticle()==DISC) {
-                Disc y = (Disc)x;
-                if(y.getInterpret()==interpret) {
-                    return y;
-                }
-            }
-        }
-        return null;
-    }
-    
-    public Pelicula obtenirPelicula(Object participant) {
-        for(Article x: llistaArticles) {
-            if(x.getTipusArticle()==PELÍCULA) {
-                Pelicula y = (Pelicula)x;
-                if(y.getLlistaActors().contains(participant)) {
-                    return y;
-                }
-            }
-        }
-        return null;
-    }
-    
-    public Llibre obtenirLlibre(String autor) {
-        for(Article x: llistaArticles) {
-            if(x.getTipusArticle()==LLIBRE) {
-                Llibre y = (Llibre)x;
-                if(y.getNomAutor().equals(autor)) {
+            if (x.getTipusArticle() == DISC) {
+                Disc y = (Disc) x;
+                if (y.getInterpret().equals(interpret)) {
                     return y;
                 }
             }
@@ -81,10 +66,59 @@ public class Magatzem {
         return null;
     }
 
+    public Pelicula obtenirPelicula(Object participant) {
+        for (Article x : llistaArticles) {
+            if (x.getTipusArticle() == PELÍCULA) {
+                Pelicula y = (Pelicula) x;
+                if (y.getLlistaActors().contains(participant)) {
+                    return y;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Llibre obtenirLlibre(String autor) {
+        for (Article x : llistaArticles) {
+            if (x.getTipusArticle() == LLIBRE) {
+                Llibre y = (Llibre) x;
+                if (y.getNomAutor().equals(autor)) {
+                    return y;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Article obtenirArticle(int referencia) {
+        for (Article x : llistaArticles) {
+            if (x.getReferencia() == referencia) {
+                return x;
+            }
+        }
+        return null;
+    }
+
+    public void addArticle(Article article) {
+        llistaArticles.add(article);
+    }
+
+    public void removeArticle(Article article) {
+       llistaArticles.remove(article);
+    }
+
+    public void disminuirQuantitat(Article y) {
+        for (Article x : llistaArticles) {
+            if (y.equals(x)) {
+                x.setQuantitat(x.getQuantitat() - 1);
+            }
+
+        }
+    }
+
     @Override
     public String toString() {
-        return "Magatzem{" + "codi=" + codi + ", nom=" + nom + ", llistaArticles=" + llistaArticles + '}';
+        return "Magatzem{" + "codi=" + codi + ", nom=" + nom + ", llistaArticles=" + llistaArticles + ", llistaVentes=" + historialVentes + '}';
     }
-    
-    
+
 }
--- src/com/canf/proves/provesCanf.java
+++ src/com/canf/proves/provesCanf.java
@@ -3,10 +3,14 @@ package com.canf.proves;
 import com.canf.excepcions.PeliculaException;
 import com.canf.articles.Article;
 import com.canf.articles.Disc;
+import com.canf.articles.Venta;
 import static com.canf.articles.tipusArticle.DISC;
+import com.canf.empresa.Magatzem;
 import com.canf.excepcions.DiscException;
 import com.canf.excepcions.ArticleException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,26 +28,20 @@ public class provesCanf {
      */
     public static void main(String[] args) {
         provesCanf p = new provesCanf();
-        p.provesXMLArticles();
-    }
-
-    public void provesXMLArticles() {
         ArrayList<Article> articles = new ArrayList<>();
-        ArrayList<String> discExclusiuBeatles = new ArrayList<>();      
+        ArrayList<Venta> ventes = new ArrayList<>();
+        ArrayList<String> discExclusiuBeatles = new ArrayList<>();  
+        Magatzem magatzem = new Magatzem(1846, "Magatzem CANF 1", articles, ventes);
         try {
-            discExclusiuBeatles.add("Love me Do");
-            Article x = new Disc("Beatles", discExclusiuBeatles,"desconeguda", 
-            "Disc de Vinil dels Beatles", "Disc de Vinil exclusiu dels Beatles", 
-            DISC, 500.0, 2);
-            
-            Article y = new Disc("", discExclusiuBeatles, "", "","", DISC, 25.0, 1);
-            articles.add(x);
-            articles.add(y);
-            x.mostraXML(articles);
-        } catch (ArticleException | DiscException e) {
-            System.out.println(e.getMessage());
+         Article x = new Disc("Beatles", discExclusiuBeatles,"desconeguda",
+                    "Disc de Vinil dels Beatles", "Disc de Vinil exclusiu dels Beatles",
+                    DISC, 500.0, 2);     
+        magatzem.addArticle(x);
+        System.out.println(magatzem);
+        } catch (ArticleException ex) {
+            Logger.getLogger(provesCanf.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (DiscException ex) {
+            Logger.getLogger(provesCanf.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
-
 }
